ee4f4f6df9b29b7ae883aa4f91861c806bc4f81a
Susumu Yamazaki <zacky1972@gmail.com>
2022-07-30 06:26:44 +0900

    remove hello

--- lib/sample_nx_add_by_gpu.ex
+++ lib/sample_nx_add_by_gpu.ex
@@ -2,17 +2,4 @@ defmodule SampleNxAddByGpu do
   @moduledoc """
   A sample program that connects Nx and GPU (CUDA or Metal).
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SampleNxAddByGpu.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
